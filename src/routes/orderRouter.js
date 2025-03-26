09b084f672f07a9f0a32f9d2bb6dc787c73b11d9
Mark Bird <markrbird13@gmail.com>
2025-03-26 15:46:42 -0600

    reset orderRouter

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -4,7 +4,7 @@ const { Role, DB } = require('../database/database.js');
 const { authRouter } = require('./authRouter.js');
 const { asyncHandler, StatusCodeError } = require('../endpointHelper.js');
 const metrics = require('../metrics.js');
-const logger = require('../logger.js');
+//const logger = require('../logger.js');
 
 const orderRouter = express.Router();
 
@@ -99,8 +99,8 @@ orderRouter.post(
       reqBody: JSON.stringify({ diner: { id: req.user.id, name: req.user.name, email: req.user.email }, order }),
       resBody: JSON.stringify(r.body),
     };
-    const level = logger.statusToLogLevel(r.status);
-    logger.log(level, 'factory', logData);
+    // const level = logger.statusToLogLevel(r.status);
+    // logger.log(level, 'factory', logData);
 
     if (r.ok) {
       metrics.trackOrders('fulfilled');
