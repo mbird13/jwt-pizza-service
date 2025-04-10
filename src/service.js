a7bd7545f8fe96bf183e05acc057c65f3b73b5d0
Mark Bird <markrbird13@gmail.com>
2025-04-10 13:55:37 -0600

    fixed linting

--- src/service.js
+++ src/service.js
@@ -33,7 +33,7 @@ apiRouter.use('/docs', (req, res) => {
   res.json({
     version: version.version,
     endpoints: [...authRouter.endpoints, ...orderRouter.endpoints, ...franchiseRouter.endpoints],
-    //config: { factory: config.factory.url, db: config.db.connection.host },
+    config: { factory: config.factory.url}, //db: config.db.connection.host },
   });
 });
 
